1fe24b47a68a0f2e49e95e615629f2018f9efbfa
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-09-01 07:38:02 +0200

    fix const alignment

--- Models/ModuleStatusUpdateType.php
+++ Models/ModuleStatusUpdateType.php
@@ -26,15 +26,15 @@ use phpOMS\Stdlib\Base\Enum;
  */
 abstract class ModuleStatusUpdateType extends Enum
 {
-    public const ACTIVATE   = 1;
+    public const ACTIVATE = 1;
 
     public const DEACTIVATE = 2;
 
-    public const INSTALL    = 3;
+    public const INSTALL = 3;
 
-    public const UNINSTALL  = 4;
+    public const UNINSTALL = 4;
 
-    public const DELETE     = 5;
+    public const DELETE = 5;
 
-    public const UPDATE     = 6;
+    public const UPDATE = 6;
 }
--- Models/PermissionState.php
+++ Models/PermissionState.php
@@ -26,21 +26,21 @@ use phpOMS\Stdlib\Base\Enum;
  */
 abstract class PermissionState extends Enum
 {
-    public const SETTINGS         = 1;
+    public const SETTINGS = 1;
 
-    public const ACCOUNT          = 2;
+    public const ACCOUNT = 2;
 
-    public const GROUP            = 3;
+    public const GROUP = 3;
 
-    public const MODULE           = 4;
+    public const MODULE = 4;
 
-    public const LOG              = 5;
+    public const LOG = 5;
 
-    public const ROUTE            = 6;
+    public const ROUTE = 6;
 
-    public const APP              = 7;
+    public const APP = 7;
 
     public const ACCOUNT_SETTINGS = 8;
 
-    public const SEARCH           = 9;
+    public const SEARCH = 9;
 }
