f3eadc8cc0e0c540aec350e07ddddc6022e06f60
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-10-16 22:25:58 +0000

    auto generate NullTests

---
+++ tests/Models/NullAccountExternalTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Admin\tests\Models;
+
+use Modules\Admin\Models\NullAccountExternal;
+
+/**
+ * @internal
+ */
+final class NullAccountExternalTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Admin\Models\NullAccountExternal
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Admin\Models\AccountExternal', new NullAccountExternal());
+    }
+
+    /**
+     * @covers Modules\Admin\Models\NullAccountExternal
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullAccountExternal(2);
+        self::assertEquals(2, $null->getId());
+    }
+
+    /**
+     * @covers Modules\Admin\Models\NullAccountExternal
+     * @group framework
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullAccountExternal(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
---
+++ tests/Models/NullDataChangeTest.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Jingga
+ *
+ * PHP Version 8.1
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 2.0
+ * @version   1.0.0
+ * @link      https://jingga.app
+ */
+declare(strict_types=1);
+
+namespace Modules\Admin\tests\Models;
+
+use Modules\Admin\Models\NullDataChange;
+
+/**
+ * @internal
+ */
+final class NullDataChangeTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @covers Modules\Admin\Models\NullDataChange
+     * @group framework
+     */
+    public function testNull() : void
+    {
+        self::assertInstanceOf('\Modules\Admin\Models\DataChange', new NullDataChange());
+    }
+
+    /**
+     * @covers Modules\Admin\Models\NullDataChange
+     * @group framework
+     */
+    public function testId() : void
+    {
+        $null = new NullDataChange(2);
+        self::assertEquals(2, $null->getId());
+    }
+
+    /**
+     * @covers Modules\Admin\Models\NullDataChange
+     * @group framework
+     */
+    public function testJsonSerialize() : void
+    {
+        $null = new NullDataChange(2);
+        self::assertEquals(['id' => 2], $null);
+    }
+}
